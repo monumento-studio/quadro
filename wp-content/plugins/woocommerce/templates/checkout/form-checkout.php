43278ee95a8c5e16156b34ac4decd9a0272c5402
Luis B <luisbttf@gmail.com>
2019-11-04 15:44:36 -0600

    v2

--- wp-content/plugins/woocommerce/templates/checkout/form-checkout.php
+++ wp-content/plugins/woocommerce/templates/checkout/form-checkout.php
@@ -43,16 +43,16 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 			<div class="col-2">
 				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
 			</div>
-		</div>
+
 
 		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
 	<?php endif; ?>
-	
+
 	<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
-	
+
 	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
-	
+	</div>
 	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
 	<div id="order_review" class="woocommerce-checkout-review-order">
